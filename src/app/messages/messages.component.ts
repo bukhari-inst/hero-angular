b7cca04f3df046931dfc5c0aab0f3b3b5d91c29e
Bukhari_Inst <bukhari414647@gmail.com>
2023-02-05 22:07:47 +0700

    Update messages.component.ts

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -4,7 +4,7 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent {
   constructor(public messageService: MessageService) {}
